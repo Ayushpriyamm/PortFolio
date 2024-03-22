91e3370828ba5d3ebb6d43aad54a5667426f5657
Ayush Priyam <ayushpriyam24@gmail.com>
2024-03-22 11:43:20 +0530

    updated projects

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,98 +1,86 @@
-import React from 'react';
-import Slider from 'react-slick';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
-import Graphics from '../assets/recentwork.svg'
+import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Graphics from "../assets/recentwork.svg";
 
-import HairBasics from '../assets/hairBasics.png'
-import Abcs from '../assets/abcs.png'
-import Tindog from '../assets/tindog.png'
-import Shirt from '../assets/shirt.png'
-import { WorkCard } from './WorkCard';
-
-
-
-
+import HairBasics from "../assets/hairBasics.png";
+import Abcs from "../assets/abcs.png";
+import Tindog from "../assets/tindog.png";
+import Shirt from "../assets/shirt.png";
+import PropertyDealer from "../assets/propertyDealer.png";
+import { WorkCard } from "./WorkCard";
 
 const Carousel = () => {
-
-  const work=[
+  const work = [
     {
-      img:HairBasics,
-      text:"Hair basics-World's Fastest Anti-Hairfall oil",
-      link:"https://basics.hair/",
+      img: Tindog,
+      text: "TinDog-Meet new and interesting dogs nearby.",
+      link: "https://ayushpriyamm.github.io/tindog/",
     },
     {
-      img:Abcs,
-      text:"ABCS INDIA-Leading financial services and solutions company.",
-      link:"https://abcsindia.in/",
-    },
-    {
-      img:Tindog,
-      text:"TinDog-Meet new and interesting dogs nearby.",
-      link:"https://ayushpriyamm.github.io/tindog/",
-    },
-    {
-      img:Shirt,
-      text:"3D-Tshirts-UNLEASH YOUR IMAGINATION and Define Your Own Style.",
-      link:"https://customtshirtdesignbyayushpriyam.shaktivriksh.com/",
+      img: Shirt,
+      text: "3D-Tshirts-UNLEASH YOUR IMAGINATION and Define Your Own Style.",
+      link: "https://customtshirtdesignbyayushpriyam.shaktivriksh.com/",
     },
 
+    {
+      img: PropertyDealer,
+      text: "Property Dealer is the bset place to find youor next perfect place to live",
+      link: "https://property-dealer.onrender.com/",
+    },
   ];
 
-    var settings = {
-      dots: false,
-      infinite: true,
-      slidesToShow: 3,
-      slidesToScroll: 1,
-      autoplay: true,
-      speed: 3000,
-      autoplaySpeed: 3000,
-      cssEase: "linear",
-      responsive: [
-        {
-          breakpoint: 768, // Define the breakpoint for mobile devices
-          settings: {
-            slidesToShow: 1, // Set the number of slides to show on mobile
-          },
+  var settings = {
+    dots: false,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    speed: 3000,
+    autoplaySpeed: 3000,
+    cssEase: "linear",
+    responsive: [
+      {
+        breakpoint: 768, // Define the breakpoint for mobile devices
+        settings: {
+          slidesToShow: 1, // Set the number of slides to show on mobile
         },
-        // You can add more breakpoints and settings as needed
-      ],
-      
-      };
+      },
+      // You can add more breakpoints and settings as needed
+    ],
+  };
 
   return (
-    <div id="recent-work" className='text-white bg-gradient-to-r from-[#544c08] via-[#201f18] to-[#191919] space-y-10 p-10 md:p-20'>
-      <div className='grid  md:grid-cols-2 gap-5 space-y-6'>
-        <div className='flex flex-col text-center md:text-left '>
-            <h2 className='text-4xl font-bold '>My Recent Work</h2>
-            <p className='text-xl' >
-            Allow me to present my recent work, 
-            where I'll provide you with a comprehensive account of my 
-            latest projects, leaving no room for mistaken ideas.
-            </p>
-        </div> 
-        <div className=' flex justify-center mx-auto my-auto items-center backdrop-saturate-200 bg-white/30 hover:bg-green-500 rounded-[50%] w-[110px] h-[110px]'>
-            <img src={Graphics} className='md:w-[95px] md:h-[95px]'  alt="" />
-            </div>
-      </div> 
+    <div
+      id="recent-work"
+      className="text-white bg-gradient-to-r from-[#544c08] via-[#201f18] to-[#191919] space-y-10 p-10 md:p-20"
+    >
+      <div className="grid  md:grid-cols-2 gap-5 space-y-6">
+        <div className="flex flex-col text-center md:text-left ">
+          <h2 className="text-4xl font-bold ">My Recent Work</h2>
+          <p className="text-xl">
+            Allow me to present my recent work, where I'll provide you with a
+            comprehensive account of my latest projects, leaving no room for
+            mistaken ideas.
+          </p>
+        </div>
+        <div className=" flex justify-center mx-auto my-auto items-center backdrop-saturate-200 bg-white/30 hover:bg-green-500 rounded-[50%] w-[110px] h-[110px]">
+          <img src={Graphics} className="md:w-[95px] md:h-[95px]" alt="" />
+        </div>
+      </div>
 
-      <div className=''>
-      <Slider {...settings}>
-      {work.map((item, index) => (
+      <div className="">
+        <Slider {...settings}>
+          {work.map((item, index) => (
             <div key={index}>
-              <WorkCard
-               img={item.img} 
-               text={item.text} 
-               link={item.link}
-               />
+              <WorkCard img={item.img} text={item.text} link={item.link} />
             </div>
           ))}
         </Slider>
       </div>
-      
     </div>
-  )
-}
+  );
+};
 
-export default Carousel
+export default Carousel;
--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,85 +1,57 @@
-import React from 'react';
-import {motion} from 'framer-motion'
+import React from "react";
+import { motion } from "framer-motion";
 
+import ButtonGreen from "./ButtonGreen";
+import Figma from "../assets/figma-icon.svg";
 
-import ButtonGreen from './ButtonGreen';
-import Figma from '../assets/figma-icon.svg';
-
-
-import MongoDB from '../assets/shield.svg';
-import ReactIcon from '../assets/react-js-icon.svg'
-import MyImg from'../assets/heroImg5.png'
-
-
+import MongoDB from "../assets/shield.svg";
+import ReactIcon from "../assets/react-js-icon.svg";
+import MyImg from "../assets/heroImg5.png";
 
 const HeroSection = () => {
-  
   return (
-    <motion.div className='py-20 bg-gradient-to-r from-[#2a181a] via-[#42211e] via-[#411d1f] via-[#41291a] via-[#493d14] via[#213839] to-[#213e46]  flex flex-col md:flex md:flex-row mx-auto justify-between  items-center p-6 md:py-20  '
-      
-    >
-        
+    <motion.div className="py-20 bg-gradient-to-r from-[#2a181a] via-[#42211e] via-[#411d1f] via-[#41291a] via-[#493d14] via[#213839] to-[#213e46]  flex flex-col md:flex md:flex-row mx-auto justify-between  items-center p-6 md:py-20  ">
       <motion.div
-      initial={{x:-1000}} 
-      animate={{x:[-1000,0]}}
-      transition={{
-        duration:3,
-        delay:0.4
-      }} 
-      id='Home' className='flex flex-col text-center md:text-left md:w-[850px]'>
-      <motion.h1
-        initial={{x:-1000}} 
-        animate={{x:[-1000,0]}}
-        transition={{
-          duration:3,
-          
-        }}
-        className='text-yellow-500 text-5xl md:text-7xl font-bold md:font-extrabold py-4'>
+        id="Home"
+        className="flex flex-col text-center md:text-left md:w-[850px]"
+      >
+        <motion.h1 className="text-yellow-500 text-5xl md:text-7xl font-bold md:font-extrabold py-4">
           Hello,
-      </motion.h1>
+        </motion.h1>
 
-        <h1 className='text-white text-5xl md:text-7xl font-bold md:font-extrabold py-4'>I am Ayush</h1>
-        <h1 className='text-green-500 text-3xl md:text-4xl font-bold py-4'>Web Developer</h1>
-        <p className='text-white   py-2'>
-        Welcome to my creative playground, where innovation meets imagination, 
-        and I'm Ayush, your friendly neighborhood digital alchemist. 
-        I've made it my mission to turn boring ideas 
-        into web wizardry that even Harry Potter would find spellbinding!
-       </p>
+        <h1 className="text-white text-5xl md:text-7xl font-bold md:font-extrabold py-4">
+          I am Ayush
+        </h1>
+        <h1 className="text-green-500 text-3xl md:text-4xl font-bold py-4">
+          Web Developer
+        </h1>
+        <p className="text-white   py-2">
+          Welcome to my creative playground, where innovation meets imagination,
+          and I'm Ayush, your friendly neighborhood digital alchemist. I've made
+          it my mission to turn boring ideas into web wizardry that even Harry
+          Potter would find spellbinding!
+        </p>
 
-        <div className='py-6'>
-          <ButtonGreen ButtonName="Hire me" href="#Contact"  />
-        
+        <div className="py-6">
+          <ButtonGreen ButtonName="Hire me" href="#Contact" />
         </div>
-        
       </motion.div>
 
-      <motion.div 
-       initial={{x:1000}} 
-       animate={{x:[1000,0]}}
-       transition={{
-         duration:2,
-         
-       }}
-      
-      className='flex justify-center items-center md:w-450'>
-        <div className='relative'>
-          <img className='rounded-[50%]   z-40' src={MyImg} alt='' />
-            
-            <div className='z-50 absolute right-0 top-[20%] w-[100px] h-[100px] p-[20px] bg-white bg-opacity-30 rounded-[25px] md:top-[15%] md:w-[95px] md:h-[95px] md:p-[15px] md:rounded-[15px] col:top-[10%] col:w-[70px] col:h-[70px] col:p-[15px] col:rounded-[15px] floating-item'>
-              <img src={Figma} alt='' />
-            </div>
-            <div className='z-50 absolute right-0 bottom-0 w-[100px] h-[100px] p-[20px] bg-black bg-opacity-30 rounded-[25px] md:bottom-[15%] md:w-[95px] md:h-[95px] md:p-[15px] md:rounded-[15px] col:top-[10%] col:w-[70px] col:h-[70px] col:p-[15px] col:rounded-[15px] floating-item'>
-              <img src={ReactIcon} alt='' />
-            </div>
-            <div className='z-50 absolute left-0 bottom-0 w-[100px] h-[100px] p-[20px] bg-white bg-opacity-30 rounded-[25px] md:bottom-[15%] md:w-[95px] md:h-[95px] md:p-[15px] md:rounded-[15px] col:top-[10%] col:w-[70px] col:h-[70px] col:p-[15px] col:rounded-[15px] floating-item'>
-              <img src={MongoDB} alt='' />
-            </div>
-          
+      <motion.div className="flex justify-center items-center md:w-450">
+        <div className="relative">
+          <img className="rounded-[50%]   z-40" src={MyImg} alt="" />
+
+          <div className="z-50 absolute right-0 top-[20%] w-[100px] h-[100px] p-[20px] bg-white bg-opacity-30 rounded-[25px] md:top-[15%] md:w-[95px] md:h-[95px] md:p-[15px] md:rounded-[15px] col:top-[10%] col:w-[70px] col:h-[70px] col:p-[15px] col:rounded-[15px] floating-item">
+            <img src={Figma} alt="" />
+          </div>
+          <div className="z-50 absolute right-0 bottom-0 w-[100px] h-[100px] p-[20px] bg-black bg-opacity-30 rounded-[25px] md:bottom-[15%] md:w-[95px] md:h-[95px] md:p-[15px] md:rounded-[15px] col:top-[10%] col:w-[70px] col:h-[70px] col:p-[15px] col:rounded-[15px] floating-item">
+            <img src={ReactIcon} alt="" />
+          </div>
+          <div className="z-50 absolute left-0 bottom-0 w-[100px] h-[100px] p-[20px] bg-white bg-opacity-30 rounded-[25px] md:bottom-[15%] md:w-[95px] md:h-[95px] md:p-[15px] md:rounded-[15px] col:top-[10%] col:w-[70px] col:h-[70px] col:p-[15px] col:rounded-[15px] floating-item">
+            <img src={MongoDB} alt="" />
+          </div>
         </div>
-        
       </motion.div>
-      
     </motion.div>
   );
 };
